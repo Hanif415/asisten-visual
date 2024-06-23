86f01d0090cd60463f5148ab6c9692f51c6b9ff1
hanif415 <hanifamrullah42@gmail.com>
2024-06-23 13:17:16 +0700

    oke

--- app/src/main/java/com/example/asistenvisual/data/response/ApiConstants.kt
+++ app/src/main/java/com/example/asistenvisual/data/response/ApiConstants.kt
@@ -1,6 +1,6 @@
 package com.example.asistenvisual.data.response
 
 object ApiConstants {
-    const val AUTHORIZATION_HEADER = "Authorization: Token r8_AIicybykswLpIQSoKg4sxCxCSCUcW9s2gMO2R"
+    const val AUTHORIZATION_HEADER = "Authorization: Token "
     const val CONTENT_TYPE_HEADER = "Content-Type: application/json"
 }
